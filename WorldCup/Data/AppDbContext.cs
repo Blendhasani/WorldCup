1f1a7e181dc39c4f01b0fb0f7d76a53a0e81f839
Blendhasani <bh51257@ubt-uni.net>
2022-12-04 21:14:35 +0100

    Krijimi i databazes, konfigurimi dhe lidhja me te dhe krijimi i fajllit AppDbContext

---
+++ WorldCup/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldCup.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
