8848e1a78fd2c842e2ba31eef2b2e7e05d1d8753
Biney07 <as51465@ubt-uni.net>
2022-12-06 04:11:25 +0100

    footer/home/navbar

---
+++ WorldCup/Models/News.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldCup.Models
+{
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(5000)]
+        public string Description { get; set; }
+        [Required]  
+        public DateTime Date { get; set; }
+    }
+}
