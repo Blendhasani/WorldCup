4f13a6ee337742db2d3c27139dbbeeb242bad504
BenettM19 <bm53089@ubt-uni.net>
2022-12-13 19:25:58 +0100

    Krijimi i metodes Edit dhe view Edit

--- WorldCup/Controllers/HighlightsController.cs
+++ WorldCup/Controllers/HighlightsController.cs
@@ -40,5 +40,26 @@ namespace WorldCup.Controllers
             var highlightsDetails =await _highlightsService.GetByIdAsync(id);
             return View(highlightsDetails);
         }
+
+        
+        public async Task<IActionResult> Edit(int id)
+        {
+            var highlightsDetails = await _highlightsService.GetByIdAsync(id);
+            if (highlightsDetails == null) return View("Not Found");
+            return View(highlightsDetails);
+        }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,ImgUrl,VideoUrl")] Highlights Highlight)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(Highlight);
+            }
+            await _highlightsService.UpdateAsync(id,Highlight);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
