7b3818875cef4d56128f75e7e769024adc71777e
Biney07 <as51465@ubt-uni.net>
2022-12-23 04:44:45 +0100

    sorting,searching -products

--- WorldCup/Controllers/ProductsController.cs
+++ WorldCup/Controllers/ProductsController.cs
@@ -8,98 +8,135 @@ using X.PagedList;
 
 namespace WorldCup.Controllers
 {
-	public class ProductsController : Controller
-	{
-		private readonly IProductsService _productsService;
-	
-		public ProductsController(IProductsService productsService)
-		{
-			_productsService= productsService;
-		}
-		public async Task<IActionResult> Index(int? page)
-		{
-		
+    public class ProductsController : Controller
+    {
+        private readonly IProductsService _productsService;
 
-			var allProducts = await _productsService.GetAllAsync(page);
-			return View(allProducts);
-		}
-
-		//get
-		public IActionResult Create()
-		{
-			return View();
-		}
-		//Get :products/Create
-
-		[HttpPost]
-		public async Task<IActionResult> Create(Product products)
-		{
-
-			if (!ModelState.IsValid)
-			{
-				return View(products);
-			}
-			
-			await _productsService.AddAsync(products);
-			return RedirectToAction(nameof(Index));
-		}
-
-		public async Task<IActionResult> Edit(int id)
-		{
-			var productDetails = await _productsService.GetByIdAsync(id);
-			if (productDetails == null) return View("NotFound");
-			return View(productDetails);
-		}
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+        public async Task<IActionResult> Index(int? page)
+        {
 
 
-		[HttpPost]
-		public async Task<IActionResult> Edit(int id,Product product)
-		{
+            var allProducts = await _productsService.GetAllAsync(page);
+            return View(allProducts);
+        }
+        public async Task<IActionResult> Sorting(string sortOrder, string searchString)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+            ViewBag.ProductTypeSortParam = sortOrder == "ProductType" ? "Product_type" : "ProductType";
+            ViewBag.PriceSortParam = sortOrder == "Price" ? "Price_type" : "Price";
+            var Products = await _productsService.GetAllAsync();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Products = Products.Where(p => p.Name.Contains(searchString));
 
-			if (!ModelState.IsValid)
-			{
-				return View(product);
-			}
-			await _productsService.UpdateAsync(id, product);
-			return RedirectToAction(nameof(Index));
-		}
-
-		public async Task<IActionResult> Details(int id)
-		{
-			var productDetails = await _productsService.GetByIdAsync(id);
-			return View(productDetails);
-		}
-
-		public async Task<IActionResult> Delete(int id)
-		{
-			var productDetails = await _productsService.GetByIdAsync(id);
-			if (productDetails == null) return View("NotFound");
-			return View(productDetails);
-		}
-
-		[HttpPost, ActionName("Delete")]
-		public async Task<IActionResult> Deleteconfirmed(int id)
-		{
-			var productDetails = await _productsService.GetByIdAsync(id);
-			if (productDetails == null) return View("NotFound");
-
-			await _productsService.DeleteAsync(id);
-			return RedirectToAction(nameof(Index));
-		}
-
-		public async Task<IActionResult> Filter(string searchString)
-		{
-
-			var allProducts = await _productsService.GetAllAsync();
-			if (!string.IsNullOrEmpty(searchString))
-			{
-				var filterResults = allProducts.Where(n => n.Name.ToLower().Contains(searchString) || n.Description.ToLower().Contains(searchString)).ToList();
-
-				return View("Filtered", filterResults);
-			}
-			return View("Filtered", allProducts);
-		}
+            }
+            switch (sortOrder)
+            {
+                case "Name":
+                    Products = Products.OrderBy(s => s.Name);
+                    break;
+                case "ProductType":
+                    Products = Products.OrderBy(s => s.ProductType);
+                    break;
+                case "Product_type":
+                    Products = Products.OrderByDescending(s => s.ProductType);
+                    break;
+                case "Price":
+                    Products = Products.OrderByDescending(s => s.Price);
+                    break;
+                case "Price_type":
+                    Products = Products.OrderBy(s => s.Price);
+                    break;
+                default:
+                    Products = Products.OrderByDescending(s => s.Name);
+                    break;
+            }
 
 
-	}
+            return View(Products.ToList());
+        }
+
+
+        //get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Get :products/Create
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product products)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(products);
+            }
+
+            await _productsService.AddAsync(products);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var productDetails = await _productsService.GetByIdAsync(id);
+            if (productDetails == null) return View("NotFound");
+            return View(productDetails);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            await _productsService.UpdateAsync(id, product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var productDetails = await _productsService.GetByIdAsync(id);
+            return View(productDetails);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productDetails = await _productsService.GetByIdAsync(id);
+            if (productDetails == null) return View("NotFound");
+            return View(productDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> Deleteconfirmed(int id)
+        {
+            var productDetails = await _productsService.GetByIdAsync(id);
+            if (productDetails == null) return View("NotFound");
+
+            await _productsService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Filter(string searchString)
+        {
+
+            var allProducts = await _productsService.GetAllAsync();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filterResults = allProducts.Where(n => n.Name.ToLower().Contains(searchString) || n.Description.ToLower().Contains(searchString)).ToList();
+
+                return View("Filtered", filterResults);
+            }
+            return View("Filtered", allProducts);
+        }
+
+
+    }
 }
