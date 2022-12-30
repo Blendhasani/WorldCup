a9eda3c29f8c5aeea5e19ace51dbb515652e5c4b
Biney07 <as51465@ubt-uni.net>
2022-12-30 02:28:08 +0100

    shopping cart added with the methodsto get all and get total

---
+++ WorldCup/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCup.Models;
+
+namespace WorldCup.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context= context;  
+        }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }   
+
+        //method to get all shopping card items
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId ==
+            ShoppingCartId).Include(n => n.Product).ToList());
+
+        }
+        //get total of shopping card items price
+        public double GetShoppingCartTotal()
+        {
+            //take the prices of the products in the list of shoppingcart
+            // add them up/
+            //show the result
+
+            var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Product.Price * n.Amount).Sum();
+
+        }
+
+    }
+}
