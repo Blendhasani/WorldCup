98f6abeb23fc7f09d90c03c2df4e52bc00019fef
eh52022 <eh52022@ubt-uni.net>
2022-12-31 14:26:34 +0100

    Krijimi i ShoppingCartViewModel dhe OrdersController

---
+++ WorldCup/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorldCup.Data.Cart;
+using WorldCup.Data.Services;
+using WorldCup.Data.ViewModels;
+
+namespace WorldCup.Controllers
+{
+	public class OrdersController : Controller
+	{
+		private readonly IProductsService _productsService;
+		private readonly ShoppingCart _shoppingCart;
+		public OrdersController(IProductsService productsService, ShoppingCart shoppingCart)
+		{
+			_productsService = productsService;	
+			_shoppingCart = shoppingCart;
+		}
+		public IActionResult Index()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+			
+			var response = new ShoppingCartVM()
+			{
+				ShoppingCart = _shoppingCart,
+				ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+
+			};
+
+			return View(response);
+		}
+	}
+}
---
+++ WorldCup/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using WorldCup.Data.Cart;
+
+namespace WorldCup.Data.ViewModels
+{
+	public class ShoppingCartVM
+	{
+		public ShoppingCart ShoppingCart { get; set; }
+		public double ShoppingCartTotal { get; set; }
+	}
+}
