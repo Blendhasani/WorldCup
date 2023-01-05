be49f523db42f9aae78af5f8d71cf363bde31f5f
eh52022 <eh52022@ubt-uni.net>
2023-01-05 23:03:16 +0100

    Sign up View and Signing up users

--- WorldCup/Controllers/AccountController.cs
+++ WorldCup/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WorldCup.Data;
+using WorldCup.Data.Static;
 using WorldCup.Data.ViewModels;
 using WorldCup.Models;
 
@@ -44,5 +45,34 @@ namespace WorldCup.Controllers
             TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
         }
-    }
+
+		public IActionResult Register() => View(new RegisterVM());
+       
+        [HttpPost]
+		public async Task<IActionResult> Register(RegisterVM registerVM)
+		{
+			if (!ModelState.IsValid) return View(registerVM);
+
+			var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+			if (user != null)
+            {
+				TempData["Error"] = "This email adress is already in use";
+                return View(registerVM); 
+            }
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if(newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            return View("RegisterCompleted");
+            
+		}
+
+
+	}
 }
